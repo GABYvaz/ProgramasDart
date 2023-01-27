68b5028fdc580fad310a5db7fb5d5e0befe96e0f
Gaby Vazquez <gaby27anahi02@outlook.es>
2023-01-27 11:33:55 -0600

    clases actualizada

--- Clases/clases/bin/clases.dart
+++ Clases/clases/bin/clases.dart
@@ -1,8 +1,20 @@
-import 'dart:html';
+import 'dart:io';
 
 void main(List<String> arguments) {
-  var unaLaptop =  Computadora(marca: 'Alienware', cpu: 'Intel core i9', ram: 32);
+  var unaLaptop =  Computadora('Alienware','Intel core i9', 32);
   print('Informacion de la laptop que te venden en internet: $unaLaptop');
+  stdout.write('¿Esta computadora es adecuada para la materia DMM?: ');
+  String? respuesta = stdin.readLineSync();
+  if(respuesta != null) {
+    if(respuesta.contains('si')){
+      unaLaptop.configurarAdecuada =true;
+    }else{
+      unaLaptop.configurarAdecuada = false;
+    }
+  }else{
+    print('No se puede saber si la PC es adecuada porque se recibio null');
+  }
+  print('Laptop1 es adecuada para tu materia: ${unaLaptop.esAdecuada}');
   unaLaptop.marca = 'Lenovo';
   unaLaptop.cpu = 'Celeron';
   unaLaptop.ram = 4;
@@ -13,6 +25,7 @@ class Computadora{
   String? marca;
   String? cpu;
   int? ram;
+  bool adecuada=false;
 
   //Constructor
   /*Computadora(String m, String c, int r){
@@ -26,7 +39,16 @@ class Computadora{
 
   @override
   String toString() {
-    return 'Marca: $marca, CPU: $cpu, RAM: $ram';//Si es variable privada se pone this._marca
+    return 'Marca: $marca, CPU: $cpu, RAM: $ram, es adecuada para DMM: $adecuada'; 
+    //Si es variable privada se pone this._marca
+  }
+  //getter de la variable adecuada
+  bool get esAdecuada{
+    return adecuada;
+  }
+  //setter de la variable adecuada
+  set configurarAdecuada(bool valor){
+    adecuada = valor;
   }
 
 }
