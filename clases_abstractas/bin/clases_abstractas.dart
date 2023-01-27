d366efbcb008417b5bff952c49ecb012dee276b6
Gaby Vazquez <gaby27anahi02@outlook.es>
2023-01-27 11:43:42 -0600

    Clases abstractas

---
+++ clases_abstractas/bin/clases_abstractas.dart
@@ -0,0 +1,27 @@
+
+import 'dart:html';
+
+void main(List<String> arguments) {
+ final unCuadrado =  Cuadrado();
+ print('El area de unCuadrado es: ${unCuadrado.area}');  
+}
+abstract class FiguraGeometrica {
+  //Estableciendo lo que debe contener una figura geometrica
+  double? area;
+
+  double calcularArea();
+}
+class Cuadrado implements FiguraGeometrica {
+  //Cuadrado hereda de figura geometrica
+  //Y debe implementar todo lo definido en la clase abstracta
+
+  @override 
+  double? area;
+
+  @override 
+  double calcularArea() {
+    double lado= 12.5;
+    return lado * lado;
+  }
+}
+
